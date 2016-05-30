d52288b8536b9ba516bba9a5d983783198402cfe
Yuval <yvw.bor@gmail.com>
2016-05-30 18:21:29 +0000

    Done..

--- server.js
+++ server.js
@@ -1,84 +1,23 @@
-//
-// # SimpleServer
-//
-// A simple chat server using Socket.IO, Express, and Async.
-//
-var http = require('http');
-var path = require('path');
 
-var async = require('async');
-var socketio = require('socket.io');
 var express = require('express');
+var app = express();
 
-//
-// ## SimpleServer `SimpleServer(obj)`
-//
-// Creates a new instance of SimpleServer with the following options:
-//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
-//
-var router = express();
-var server = http.createServer(router);
-var io = socketio.listen(server);
-
-router.use(express.static(path.resolve(__dirname, 'client')));
-var messages = [];
-var sockets = [];
-
-io.on('connection', function (socket) {
-    messages.forEach(function (data) {
-      socket.emit('message', data);
-    });
-
-    sockets.push(socket);
-
-    socket.on('disconnect', function () {
-      sockets.splice(sockets.indexOf(socket), 1);
-      updateRoster();
-    });
-
-    socket.on('message', function (msg) {
-      var text = String(msg || '');
-
-      if (!text)
-        return;
-
-      socket.get('name', function (err, name) {
-        var data = {
-          name: name,
-          text: text
-        };
-
-        broadcast('message', data);
-        messages.push(data);
-      });
-    });
-
-    socket.on('identify', function (name) {
-      socket.set('name', String(name || 'Anonymous'), function (err) {
-        updateRoster();
-      });
-    });
+app.route('/api/whoami')
+  .get((req, res) => {
+    res.writeHead(200, {'content-type': 'text/json'});
+    res.end(JSON.stringify({
+      ipaddress: req.headers['x-forwarded-for'],
+      language: req.headers['accept-language'].split(',')[0],
+      'user-agent': req.headers['user-agent']
+    }));
   });
+  
+app.route('/')
+  .all((req, res) => {
+    res.writeHead(200, {'content-type': 'text/plain'});
+    res.end("Hello.. Please go to /api/whoami. Thank you.");
+  })
 
-function updateRoster() {
-  async.map(
-    sockets,
-    function (socket, callback) {
-      socket.get('name', callback);
-    },
-    function (err, names) {
-      broadcast('roster', names);
-    }
-  );
-}
-
-function broadcast(event, data) {
-  sockets.forEach(function (socket) {
-    socket.emit(event, data);
-  });
-}
-
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  var addr = server.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
+app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  console.log("Server listening at " + process.env.IP + ' port ' + process.env.port);
 });
